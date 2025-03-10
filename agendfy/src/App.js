218d60c1f68736f5c4768d7dbc2a1c9369986196
GuiGavassa <guilhermegavassa.tecn@gmail.com>
2025-03-10 20:19:47 -0300

    css att para tela notebook

--- agendfy/src/App.js
+++ agendfy/src/App.js
@@ -11,18 +11,7 @@ function App() {
   return (
     <div className="App">
       <header className="app-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        
         <img src={logo} id="agendfy_logo" alt="" />
         <ul>
           <li><a href="#home">Home</a></li>
@@ -41,8 +30,10 @@ function App() {
         <div>
           <h1>Gerencie a agenda de sua empresa conosco!</h1>
           <p>Aumente a eficiência de seu negócio com a nossa tecnologia em gerenciamento de agendamentos. </p>
-          <button id="btn_primario">Saiba Mais</button>
-          <button id="btn_secundario">Criar Conta</button>
+          <div className="div-buttons">
+            <button id="btn_primario">Saiba Mais</button>
+            <button id="btn_secundario">Criar Conta</button>
+          </div>
         </div>
         <img src={ImgHome} className="card-img" alt="" />
       </section>
@@ -60,19 +51,19 @@ function App() {
           <img src={ImgHome} className="card-img" alt="" />
           <div className="cards-solucao">
             <div>
-              <img src={CardIcon1} className="card-img" alt="" />
+              <img src={CardIcon1} className="card-icon" alt="" />
               <h5>Facilite o gerenciamento de agendas com a Dashboard customizada!</h5>
             </div>
             <div>
-              <img src={CardIcon2} className="card-img" alt="" />
+              <img src={CardIcon2} className="card-icon" alt="" />
               <h5>Tenha mais eficiência nos atendimentos aos seus clientes.</h5>
             </div>
             <div>
-              <img src={CardIcon3} className="card-img" alt="" />
+              <img src={CardIcon3} className="card-icon" alt="" />
               <h5>Organize seus agendamentos em calendários semanais</h5>
             </div>
             <div>
-              <img src={CardIcon4} className="card-img" alt="" />
+              <img src={CardIcon4} className="card-icon" alt="" />
               <h5>Aumente a produtividade de seus serviços utilizando a nossa solução.</h5>
             </div>
           </div>
