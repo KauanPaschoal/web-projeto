90c3a1fc6ab8b754d2d75f39e8860fbb668053fa
GuiGavassa <guilhermegavassa.tecn@gmail.com>
2025-03-20 09:51:13 -0300

    criado arquivo para landing page

---
+++ web-agendfy/src/assets/pages/landingPage/LandingPage.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+import LogoBranca from '../../images/LogoTipo Branco 1.svg'
+
+import DivisaoComponent from './components/divisao/divisao'
+import HomeComponent from './home/HomeComponent'
+import NossaEssenciaComponent from './nossa-essencia/NossaEssenciaComponent'
+import SolucaoComponent from './solucao/SolucaoComponent';
+import RecursosComponent from './recursos/RecursosComponent';
+import HeaderComponent from './header/HeaderComponent';
+
+const LandingPage = () => {
+    return (
+        <div>
+            <HeaderComponent></HeaderComponent>
+            {/* Seção Home */}
+            <HomeComponent></HomeComponent>
+
+            <DivisaoComponent></DivisaoComponent>
+
+            <SolucaoComponent></SolucaoComponent>
+
+            <DivisaoComponent></DivisaoComponent>
+
+            <NossaEssenciaComponent></NossaEssenciaComponent>
+
+            <DivisaoComponent></DivisaoComponent>
+
+            <RecursosComponent></RecursosComponent>
+
+            <DivisaoComponent></DivisaoComponent>
+
+            <section className="section-contate" id="contate">
+
+            </section>
+
+            <footer>
+                <img src={LogoBranca} alt="" />
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#solucao">Solução</a></li>
+                    <li><a href="#essencia">Nossa Essência</a></li>
+                    <li><a href="#recursos">Recursos AgendFy</a></li>
+                    <li><a href="#contate">Contate-nos</a></li>
+                </ul>
+                <p>AgendFy @ 2025. Todos os direitos reservados.</p>
+            </footer>
+        </div>
+    )
+}
+
+export default LandingPage
