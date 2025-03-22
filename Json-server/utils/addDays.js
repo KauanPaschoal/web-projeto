76d457f90c724e632fc93b7d4da637f356571b00
KauanPaschoal <kauan.psantos@sptech.school>
2025-03-22 20:28:24 -0300

    🧹 cleanup: Removendo comentários desnecessários

--- Json-server/utils/addDays.js
+++ Json-server/utils/addDays.js
@@ -1,8 +1,8 @@
 function addDays(date, days) {
-	const result = new Date(date);
-	result.setDate(result.getDate() + days);
-	return result;
+  const result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
 }
- 
+
 const date = new Date();
-console.log(addDays(date, 5)); // Adds 5 days to the current date
+console.log(addDays(date, 5));
