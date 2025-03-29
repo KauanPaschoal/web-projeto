202ee0e7d2606dfbbfc70975ff6752e3166d8809
KauanPaschoal <kauan.psantos@sptech.school>
2025-03-29 20:02:24 -0300

    feat: atualizando componente de login e adicionando logo

--- web-agendfy/src/pages/Login/loginpage/loginComponent.jsx
+++ web-agendfy/src/pages/Login/loginpage/loginComponent.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { autenticateUser } from "../../../utils/auth.js"; // Importa a função de autenticação
 import { responseMessage } from "../../../utils/alert.js"; // Importa a função de sucesso
 import Swal from "sweetalert2";
+import logo from "../../../assets/images/LogoTipo Branco 1.svg";
 
 import "./login.css";
 
@@ -23,15 +24,12 @@ const Login = () => {
   return (
     <div className="background">
       <div className="card">
-        <div className="login-container flex flex-col items-center w-1/2 p-12">
-          <Link
-            to="/"
-            className="text-lightBlueFy text-lg font-semibold text-left"
-          >
+        <div className="login-container">
+          <Link to="/" className="">
             Voltar
           </Link>
-          <h1 className="text-3xl font-semibold">Que bom te ver de novo!</h1>
-          <h2 className="text-2xl font-medium">Entre para continuar</h2>
+          <h1 className="text-2xl font-semibold">Que bom te ver de novo!</h1>
+          <h2 className="text-1xl font-small">Entre para continuar</h2>
           <form
             id="loginForm"
             onSubmit={handleSubmit}
@@ -39,7 +37,7 @@ const Login = () => {
             className="w-4/5 space-y-8"
           >
             <div>
-              <label htmlFor="email" className="block text-base font-medium">
+              <label htmlFor="email" className="block text-base font-small">
                 E-mail
               </label>
               <input
@@ -66,7 +64,7 @@ const Login = () => {
                 className="w-full p-4 border-b-2 border-lightGrayFy"
               />
             </div>
-            <p className="text-sm text-gray-600">
+            <p className="">
               É necessário no mínimo 8 letras, 1 letra maiúscula e 1 número.
             </p>
             <Link
@@ -87,15 +85,8 @@ const Login = () => {
             Google
           </button>
         </div>
-        <div className="cadastro-container flex flex-col items-center w-1/2 p-12 bg-darkestBlueFy text-white space-y-4">
-          <h1 className="text-3xl font-semibold">Não tem uma conta?</h1>
-          <h3 className="text-xl">Inicie o Cadastro</h3>
-          <Link
-            to="/cadastro"
-            className="bg-transparent text-white text-lg font-semibold py-2 px-8 border-2 border-white hover:bg-white hover:text-darkBlueFy transition ease-in-out duration-200"
-          >
-            Cadastrar
-          </Link>
+        <div className="cadastro-container bg-darkestBlueFy">
+          <img src={logo} alt="Logo" className="logo-class" />
         </div>
       </div>
     </div>
