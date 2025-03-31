19e96c845f28232f652bbf5f7a1fc1574a76633f
KaueSilvaDev <kaue.o.silva35@gmail.com>
2025-03-31 20:18:24 -0300

    add: adicionarPaciente path

--- web-agendfy/src/App.jsx
+++ web-agendfy/src/App.jsx
@@ -6,6 +6,8 @@ import Login from "./pages/Login/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Agendamentos from "./pages/dashboard/agendamentos/Agendamentos";
 import Pacientes from "./pages/dashboard/pacientes/Pacientes";
+import EditarPaciente from "./pages/dashboard/pacientes/EditarPaciente/EditarPaciente";
+import AdicionarPaciente from "./pages/dashboard/pacientes/AdicionarPaciente/AdicionarPaciente";
 import Administracao from "./pages/dashboard/administração/Administracao";
 
 function App() {
@@ -20,6 +22,8 @@ function App() {
             <Route path="/dashboard/agendamentos" element={<Agendamentos />} />
             <Route path="/dashboard/pacientes" element={<Pacientes />} />
             <Route path="/dashboard/administracao" element={<Administracao />} />
+            <Route path="/dashboard/pacientes/editar" element={<EditarPaciente />} />
+            <Route path="/dashboard/pacientes/adicionar" element={<AdicionarPaciente />} />
             <Route path="*" element={<h1>Página não encontrada.</h1>} />
           </Routes>
         </BrowserRouter>
