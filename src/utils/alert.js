ca572fad79b08c704afaaa56afa5c51edbef5cd9
KauanPaschoal <kauan.psantos@sptech.school>
2025-04-05 14:20:35 -0300

    feat: implementando funcionalidade de login com autenticação de usuário e interface aprimorada

--- src/utils/alert.js
+++ src/utils/alert.js
@@ -1,23 +1,30 @@
 import Swal from "sweetalert2";
+import "../App.css";
 
-export function errorMessage(mensagem) {
-    Swal.fire({
-      position: "top-end",
-      icon: "error",
-      title: mensagem,
-      showConfirmButton: false,
-      timer: 2300,
-      backdrop: false,
-    });
-  }
-  
-export function responseMessage(mensagem) {
-    Swal.fire({
-      position: "top-end",
-      icon: "success",
-      title: mensagem,
-      showConfirmButton: false,
-      timer: 2300,
-      backdrop: false,
-    });
-  }
+export function errorMessage(mensagem, size = "small") {
+  Swal.fire({
+    position: "top-end",
+    icon: "error",
+    title: mensagem,
+    showConfirmButton: false,
+    timer: 2300,
+    backdrop: false,
+    customClass: {
+      popup: `swal-${size}`,
+    },
+  });
+}
+
+export function responseMessage(mensagem, size = "small") {
+  Swal.fire({
+    position: "top-end",
+    icon: "success",
+    title: mensagem,
+    showConfirmButton: false,
+    timer: 2300,
+    backdrop: false,
+    customClass: {
+      popup: `swal-${size}`,
+    },
+  });
+}
