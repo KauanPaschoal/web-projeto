a7aa6f178e7c02dba7a56855c3c09ec59f42c2e2
KauanPaschoal <kauan.psantos@sptech.school>
2025-04-21 19:29:31 -0300

    feat: Refatorar InputField e MenuLateralComponent para melhor estrutura e usabilidade

--- src/pages/dashboard/components/InputField/InputField.jsx
+++ src/pages/dashboard/components/InputField/InputField.jsx
@@ -1,30 +1,43 @@
-import React from 'react'
-import './inputField.css'
+import React from "react";
+import "./inputField.css";
 
+const InputField = ({
+  labelTitle,
+  type = "text",
+  placeholder,
+  id,
+  name,
+  onChange,
+  onBlur,
+  list,
+  required,
+  className,
+  value,
+  icon,
+  width,
+  disabled,
+}) => {
+  return (
+    <div className={`inputContainer ${width || "w-[45%]"}`}>
+      <label>{labelTitle}:</label>
+      <div className="input-with-icon">
+        {icon && <span className="input-icon">{icon}</span>}
+        <input
+          type={type}
+          id={id}
+          name={name}
+          placeholder={placeholder}
+          onChange={onChange}
+          onBlur={onBlur}
+          list={list}
+          required={required}
+          value={value}
+          className={`inputField ${className}`}
+          disabled={disabled}
+        />
+      </div>
+    </div>
+  );
+};
 
-const InputField = ({labelTitle, type = 'text', placeholder, id, name, onChange, onBlur, list, required, className, value, icon, width}) => {
-
-    return (
-        <div className= {`inputContainer ${width}`} >
-            <label>{labelTitle}:</label>
-            <div className="input-with-icon">
-                {icon && <span className="input-icon">{icon}</span>}
-                <input
-                    type={type}
-                    id={id}
-                    name={name}
-                    placeholder={placeholder}
-                    onChange={onChange}
-                    onBlur={onBlur}
-                    list={list}
-                    required={required}
-                    className={`inputField ${className}` }
-                    value={value}
-
-                />
-            </div>
-        </div>
-    )
-}
-
-export default InputField
+export default InputField;
