b28acc37ad3f441adbf172dab7d98b6343702fd9
KauanPaschoal <kauan.psantos@sptech.school>
2025-06-12 00:04:11 -0300

    refactor: melhora a formatação e a legibilidade do componente AgendaDiaComponent

--- src/pages/dashboard/components/AgendaDiaComponent/AgendaDiaComponent.jsx
+++ src/pages/dashboard/components/AgendaDiaComponent/AgendaDiaComponent.jsx
@@ -1,46 +1,50 @@
-import React from 'react'
-import './AgendaDiaComponent.css'
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import "./AgendaDiaComponent.css";
+import { useNavigate } from "react-router-dom";
 
-const AgendaDiaComponent = ({ nome, horario, status = 'Default', id }) => {
+const AgendaDiaComponent = ({ nome, horario, status = "Default", id }) => {
   const navigate = useNavigate();
 
+  const getStatusClasse = () => {
+    switch (status) {
+      case "CONFIRMADA":
+        return "status-confirmado";
+      case "PENDENTE":
+        return "status-pendente";
+      case "CANCELADO":
+        return "status-cancelado";
+      case "CONCLUIDA":
+        return "status-concluido";
+      default:
+        return "status-default";
+    }
+  };
 
-      const getStatusClasse = () => {
-        switch (status) {
-          case 'CONFIRMADA':
-            return 'status-confirmado';
-          case 'PENDENTE':
-            return 'status-pendente';
-          case 'CANCELADO':
-            return 'status-cancelado';
-          default:
-            return 'status-default';
-        }
-      };
+  const handleButtonClick = (e) => {
+    e.stopPropagation();
+    navigate(`/dashboard/agendamentos/editar/${id}`);
+  };
 
-     const handleButtonClick = (e) => {
-      e.stopPropagation();
-      navigate(`/dashboard/agendamentos/editar/${id}`);
-     }
-
-    return (
-        <div className='agendaDiaComponent bg-gray-200'>
-            <span className={`status ${getStatusClasse()}`}>{status}</span>
-            <div className='info-card flex'>
-                <div className='flex gap-1'>
-                    <span className='font-bold'>Nome:</span><p>{nome}</p>
-                </div>
-            </div>
-            <div className='info-card flex'>
-                <div className='flex gap-1'>
-                    <span className='font-bold'>Horário:</span><p>{horario}</p>
-                </div>
-                
-            </div>
-            <button className='btn_primario rounded-full'onClick={handleButtonClick}>Ver Detalhes</button>
+  return (
+    <div className="agendaDiaComponent bg-gray-200">
+      <span className={`status ${getStatusClasse()}`}>{status}</span>
+      <div className="info-card flex">
+        <div className="flex gap-1">
+          <span className="font-bold">Nome:</span>
+          <p>{nome}</p>
         </div>
-    )
-}
+      </div>
+      <div className="info-card flex">
+        <div className="flex gap-1">
+          <span className="font-bold">Horário:</span>
+          <p>{horario}</p>
+        </div>
+      </div>
+      <button className="btn_primario rounded-full" onClick={handleButtonClick}>
+        Ver Detalhes
+      </button>
+    </div>
+  );
+};
 
-export default AgendaDiaComponent
+export default AgendaDiaComponent;
