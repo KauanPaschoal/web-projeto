c1469d027f648a9c3b194c39c1f9455b752b11fc
GuiGavassa <guilhermegavassa.tecn@gmail.com>
2025-08-22 16:40:53 -0300

    ajuste caminho environment

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { environment } from "../../../environments/environment";
+import { environment } from "../../environments/environment";
 
 const ProtectedRoute = ({ children }) => {
   if (environment.env !== "dev") {
